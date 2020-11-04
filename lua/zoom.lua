e5b9c288df5ce941cc2ea02ee3d62691dca7126c
Brian Shu <littlebubu.shu@gmail.com>
2020-11-03 20:33:56 -0500

    started git

---
+++ lua/zoom.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+-- the zoom state
+-- zoomed created zoom winid : previous winid
+local saved_winids = {}
+
+-- get a new table the contains the zoom_tab : previous_tab
+-- must have separate function that calls on the fly because tab numbers can change but winid's can't
+local function get_tabs()
+  local res = {}
+  for zoom_winid, previous_winid in pairs(saved_winids) do
+    local zoom_tab = vim.fn.win_id2tabwin(zoom_winid)[1]
+    local previous_tab = vim.fn.win_id2tabwin(previous_winid)[1]
+    res[zoom_tab] = previous_tab
+  end
+  return res
+end
+
+local function get_current_tab_info()
+  local current_tabnr = vim.fn.tabpagenr()
+  return vim.fn.gettabinfo(current_tabnr)
+end
+
+function M.zoom()
+  local current_tabnr = vim.fn.tabpagenr()
+  local tabs = get_tabs()
+  if tabs[current_tabnr] == nil then
+    local previous_winid = vim.fn.win_getid()
+
+    local bufname = api.nvim_buf_get_name(0)
+    if bufname == nil or bufname == "" then
+      vim.cmd [[tabnew]]
+    else
+      vim.cmd('tabedit ' .. bufname)
+    end
+
+    local new_tabnr = vim.fn.win_getid()
+
+    saved_winids[new_tabnr] = previous_winid
+  else
+    local goto_tabnr = tabs[current_tabnr]
+    vim.cmd [[tabclose]]
+    vim.cmd('tabn ' .. goto_tabnr)
+    saved_winids[vim.fn.win_getid()] = nil
+  end
+end
+
+return M
